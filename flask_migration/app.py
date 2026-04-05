da0145f8975098cbb6b9346a56dbcdfdd8ec5cc0
Vanessa Lucena <vaane.lucena@gmail.com>
2026-04-05 12:41:00 -0300

    Fix deploy: gunicorn chdir e disco persistente

--- flask_migration/app.py
+++ flask_migration/app.py
@@ -18,7 +18,7 @@ from blueprints.professor import professor_bp
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
-    app.secret_key = "chave_secreta_dev_2024"   # trocar em produção
+    app.secret_key = os.environ.get("SECRET_KEY", "chave_secreta_dev_2024")
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(aluno_bp)
